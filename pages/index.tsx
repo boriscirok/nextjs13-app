e957bb7fc04b608d1862e2fc1941b8e0f6453ad6
Boris CIROK <boris.cirok@tipsport.cz>
2023-02-16 21:26:13 +0100

    init
    init typescript

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  )
+}
